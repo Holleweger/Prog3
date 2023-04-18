303e66836441d7e182acef28bc4bf7d89e7bbb65
Wagner Carminatti Holleweger <387039@unoesc.lan>
2023-04-18 20:51:42 -0300

    18/04/2023

---
+++ News/News/News/Settings.cs
@@ -0,0 +1,13 @@
+﻿namespace News
+{
+    public static class Settings
+    {
+        public static string NewsApiKey
+        {
+            get
+            {
+                return "0b157cb8af13441386ad60435bcfa501";
+            }
+        }
+    }
+}
