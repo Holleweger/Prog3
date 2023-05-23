79789fbe611cd3207e3f4ea3a7df5d79a5802f7b
Wagner Carminatti Holleweger <387039@unoesc.lan>
2023-05-23 19:47:54 -0300

    '23/05/2023'

---
+++ Atividades/News/News/News/Views/HeadLinesView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using News.Services;
+using News.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace News.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HeadLinesView : ContentPage
+    {
+        public HeadLinesView()
+        {
+            InitializeComponent();
+            Task.Run(async () => await Initialize("Headlines"));
+        }
+        public HeadLinesView(string scope)
+        {
+            InitializeComponent();
+            Title = $"{scope} news";
+            Task.Run(async () => await Initialize(scope));
+        }
+        private async Task Initialize(string scope)
+        {
+            var viewModel = Resolver.Resolve<HeadlinesViewModel>();
+            BindingContext = viewModel;
+            await viewModel.Initialize(scope);
+        }
+    }
+}
